b16791ca892178799ff5f80b68784bd615f67514
mgrztwnk5@gmail.com <mgrztwnk5@gmail.com>
2022-01-14 22:29:19 +0100

    Create DeleteForeign_keys.sql
    Usunięcie wszystkich kluczy obcych.

---
+++ database/DeleteForeign_keys.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `Client` DROP FOREIGN KEY `client_ibfk_1` ;
+ALTER TABLE `Groups` DROP FOREIGN KEY `groups_ibfk_1` ;
+ALTER TABLE `Groups` DROP FOREIGN KEY `groups_ibfk_2` ;
+ALTER TABLE `Project` DROP FOREIGN KEY `project_ibfk_1` ;
+ALTER TABLE `Project` DROP FOREIGN KEY `project_ibfk_2` ;
+ALTER TABLE `Project` DROP FOREIGN KEY `project_ibfk_3` ;
+ALTER TABLE `Task` DROP FOREIGN KEY `task_ibfk_3` ;
+ALTER TABLE `Task` DROP FOREIGN KEY `task_ibfk_4` ;
